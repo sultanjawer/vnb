31b629411db07b3ceb62744c1b6e5ec8e2e41eae
RickySBahaso <ricky@sali-evo.com>
2023-03-24 14:53:40 +0700

    update ke 9

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,39 +1,29 @@
-@extends('layouts.admin')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-@include('partials.breadcrumb')
-@include('partials.subheader')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
 
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-password-form')
+                </div>
+            </div>
 
-<div class="row">
-	<div class="col-md-12">
-		<div class="panel" id="panel-1">
-			<div class="panel-container show">
-				<div class="panel-content">
-					@include('profile.partials.update-profile-information-form')
-				</div>
-			</div>
-		</div>
-		<div class="panel" id="panel-2">
-			<div class="panel-container show">
-				<div class="panel-content">
-					@include('profile.partials.update-password-form')
-				</div>
-			</div>
-		</div>
-		{{-- <div class="panel" id="panel-3">
-			<div class="panel-container show">
-				<div class="panel-content">
-					@include('profile.partials.delete-user-form')
-				</div>
-			</div>
-		</div> --}}
-	</div>
-</div>
-
-@endsection
-
-@section('scripts')
-@parent
-
-@endsection
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.delete-user-form')
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
